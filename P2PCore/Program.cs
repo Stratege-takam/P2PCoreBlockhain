beac318a65abe8566cc9b725e9122571e594db30
Stratege <213346@supinfo.com>
2019-03-10 20:00:05 +0100

    personalisation des erreurs

--- P2PCore/Program.cs
+++ P2PCore/Program.cs
@@ -13,76 +13,92 @@ namespace P2PCore
 
 		static void Main(string[] args)
 		{
-			// creer le block genis
-			PhillyCoin.InitializeChain();
-
-			if (args==null || args.Length==0)
+			var Quict = false;
+			while (!Quict)
 			{
-				Console.WriteLine("Entrer le port :  ");
-				Port = int.Parse( Console.ReadLine());
-
-				Console.WriteLine("Le nom de l'emmetteur :  ");
-				name = Console.ReadLine();
-			}
-
-			//si le port existe 
-			if (args.Length >= 1)
-				Port = int.Parse(args[0]);
-			// si le nom existe
-			if (args.Length >= 2)
-				name = args[1];
-
-			if (Port > 0)
-			{
-				Server = new P2PServer();
-				Server.Start();
-			}
-			if (name != "Unkown")
-			{
-				Console.WriteLine($"Current user is {name}");
-			}
-
-			Console.WriteLine("=========================");
-			Console.WriteLine("1. Connect to a server");
-			Console.WriteLine("2. Add a transaction");
-			Console.WriteLine("3. Display Blockchain");
-			Console.WriteLine("4. Exit");
-			Console.WriteLine("=========================");
-
-			int selection = 0;
-			while (selection != 4)
-			{
-				switch (selection)
+				try
 				{
-					case 1:
-						Console.WriteLine("Please enter the server URL");
-						string serverURL = Console.ReadLine();
-						Client.Connect($"{serverURL}/Blockchain");
-						break;
-					case 2:
-						Console.WriteLine("Please enter the receiver name");
-						string receiverName = Console.ReadLine();
-						Console.WriteLine("Please enter the amount");
-						string amount = Console.ReadLine();
-						//creer une transaction
-						PhillyCoin.CreateTransaction(new Transaction(name, receiverName, int.Parse(amount)));
-						// créer un block, ajouter la transaction en attente 
-						PhillyCoin.ProcessPendingTransactions(name);
-						Client.Broadcast(JsonConvert.SerializeObject(PhillyCoin));
-						break;
-					case 3:
-						Console.WriteLine("Blockchain");
-						Console.WriteLine(JsonConvert.SerializeObject(PhillyCoin, Formatting.Indented));
-						break;
+					// creer le block genis
+					PhillyCoin.InitializeChain();
 
+					if (args == null || args.Length == 0)
+					{
+						Console.WriteLine("Entrer le port :  ");
+						Port = int.Parse(Console.ReadLine());
+
+						Console.WriteLine("Le nom de l'emmetteur :  ");
+						name = Console.ReadLine();
+					}
+
+					//si le port existe 
+					if (args.Length >= 1)
+						Port = int.Parse(args[0]);
+					// si le nom existe
+					if (args.Length >= 2)
+						name = args[1];
+
+					if (Port > 0)
+					{
+						Server = new P2PServer();
+						Server.Start();
+					}
+					if (name != "Unkown")
+					{
+						Console.WriteLine($"Current user is {name}");
+					}
+
+					Console.WriteLine("=========================");
+					Console.WriteLine("1. Connect to a server");
+					Console.WriteLine("2. Add a transaction");
+					Console.WriteLine("3. Display Blockchain");
+					Console.WriteLine("4. Exit");
+					Console.WriteLine("=========================");
+
+					int selection = 0;
+					while (selection != 4)
+					{
+						switch (selection)
+						{
+							case 1:
+								Console.WriteLine("Please enter the server URL");
+								string serverURL = Console.ReadLine();
+								Client.Connect($"{serverURL}/Blockchain");
+								break;
+							case 2:
+								Console.WriteLine("Please enter the receiver name");
+								string receiverName = Console.ReadLine();
+								Console.WriteLine("Please enter the amount");
+								string amount = Console.ReadLine();
+								//creer une transaction
+								PhillyCoin.CreateTransaction(new Transaction(name, receiverName, int.Parse(amount)));
+								// créer un block, ajouter la transaction en attente 
+								PhillyCoin.ProcessPendingTransactions(name);
+								Client.Broadcast(JsonConvert.SerializeObject(PhillyCoin));
+								break;
+							case 3:
+								Console.WriteLine("Blockchain");
+								Console.WriteLine(JsonConvert.SerializeObject(PhillyCoin, Formatting.Indented));
+								break;
+							default:
+								Quict = true;
+								break;
+
+						}
+
+						Console.WriteLine("Please select an action");
+						string action = Console.ReadLine();
+						selection = int.Parse(action);
+					}
+
+					Client.Close();
 				}
-
-				Console.WriteLine("Please select an action");
-				string action = Console.ReadLine();
-				selection = int.Parse(action);
+				catch (Exception ex)
+				{
+					Console.WriteLine($"une erreur survenue : {ex.Message} ");
+					Quict = false;
+				}
 			}
-
-			Client.Close();
+			
 		}
 	}
 }
