9875643c808e2137fdbc849cc5c6826b8a35ba6a
Stratege <213346@supinfo.com>
2019-03-10 19:46:29 +0100

    Suite implementation de la blockchain peertopeer

---
+++ P2PCore/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2PCore
+{
+    public class Transaction
+    {
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+        public int Amount { get; set; }
+
+        public Transaction(string fromAddress, string toAddress, int amount)
+        {
+            FromAddress = fromAddress;
+            ToAddress = toAddress;
+            Amount = amount;
+        }
+    }
+}
